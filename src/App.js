a512126b60d82a52c87c33d81233bb75a97febe7
RyleGison <rylesirach.gison@uap.asia>
2023-12-06 16:51:27 +0800

    Final Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React, { useState } from 'react';
 import Student from './Student.jsx'
-import profile1 from './assets/student-1.jpg'
-import profile2 from './assets/student-2.jpg'
-import profile3 from './assets/student-3.jpg'
-import profile4 from './assets/student-4.jpg'
+import profile1 from './assets/student-1.jpg';
+import profile2 from './assets/student-2.jpg';
+import profile3 from './assets/student-3.jpg';
+import profile4 from './assets/student-4.jpg';
 
 function App() {
+  const [selectedStud, setSelectedStud] = useState(null);
     return(
       <>
-        <Student profilePic={profile1} name="Spongebob" age={30} isStudent={true}/>
-        <Student profilePic={profile2} name="Patrick" age={42} isStudent={false}/>
-        <Student profilePic={profile3} name="Squidward" age={50} isStudent={false}/>
-        <Student profilePic={profile4} name="Sandy" age={27} isStudent={true}/>
+        <Student profilePic={profile1} studentNum="1001" name="Spongebob" birth="July 14" quote="I'm, I'm, I'm- I'M A GOOFY GOOBER." Selected={selectedStud === "1001"} onClick={() => setSelectedStud("1001")}/>
+        <Student profilePic={profile2} studentNum="1002" name="Finn" birth="March 14" quote="With you, I would walk down any road, milady—especially if it leads to the movies." Selected={selectedStud === "1002"} onClick={() => setSelectedStud("1002")}/>
+        <Student profilePic={profile3} studentNum="1003" name="Mordecai" birth="January 15" quote="Dude, you can’t run from the future!" Selected={selectedStud === "1003"} onClick={() => setSelectedStud("1003")}/>
+        <Student profilePic={profile4} studentNum="1004" name="Gumball" birth="July 24" quote="That's not what you are, it's just how you feel." Selected={selectedStud === "1004"} onClick={() => setSelectedStud("1004")}/>
       </>
     );
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,25 +1,33 @@
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 function Student(props){
     return(
-        <div className="student">
+        <div className={`student ${props.Selected ? 'selected' : ''}`} onClick={props.onClick}>
             <img className="card-image" src={props.profilePic} alt="profile picture"/>
-            <p>Name: {props.name}</p>
-            <p>Age: {props.age}</p>
-            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+            <div className="info">
+                <p>Student ID: {props.studentNum} </p>
+                <p>Name: {props.name}</p>
+                <p>Birthday: {props.birth}</p>
+                <p>Quotable Quotes: "{props.quote}"</p>
+            </div>
         </div>
     );
 }
 Student.propTypes ={
     profilePic: PropTypes.string,
+    studentNum: PropTypes.string,
     name: PropTypes.string,
-    age: PropTypes.number,
-    isStudent: PropTypes.bool,
+    birth: PropTypes.string,
+    quote: PropTypes.string,
+    Selected: PropTypes.bool,
+    onClick: PropTypes.func,
 }
 Student.defaultPops = {
     profilePic: "./assets/student-1.jpg",
+    studentNum: "0000", 
     name: "Guest",
-    age: 0,
-    isStudent: false,
+    birth: "January 1",
+    quote: "none",
 };
 export default Student
